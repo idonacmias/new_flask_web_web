13aec8e903656738b07badb5ee5ee101c5187d90
idonacmias <idonacmi@gmail.com>
2022-11-18 11:40:07 +0200

    copied flask file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask	
-
-
+import json
+from flask import Flask
+from flask_cors import CORS
+ 
 app = Flask(__name__)
-
+CORS(app)
+ 
+ar=[{"name":"betty","age":20},{"name":"alex","age":21},{"name":"shadi","age":15}]
+ 
 @app.route('/')
-def index():
-	return 'index'
-
-
+def hello():
+    return json.dumps( ar)
+ 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
